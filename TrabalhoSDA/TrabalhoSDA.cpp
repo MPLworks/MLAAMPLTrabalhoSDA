68a0d44f6edc17353b34257b5328e14e80b9e7d2
Malu <ma.luizalves@gmail.com>
2021-02-23 22:49:15 -0300

    Inicio

--- TrabalhoSDA/TrabalhoSDA.cpp
+++ TrabalhoSDA/TrabalhoSDA.cpp
@@ -1,20 +1,87 @@
-// TrabalhoSDA.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+// Disciplina: Sistemas Distribuidos para Automacao
 //
+// Programa-cliente para simular o Sistema de Otimização de carregamento de Vagões - Trabalho Final - 2020/2
+//
+// Maria Luiza de Andrade Alves e Matheus Paiva Loures
 
+#define _CRT_SECURE_NO_WARNINGS 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS //Uso necessário devido a função inet_addr
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#define WIN32_LEAN_AND_MEAN
+#define _WIN32_WINNT  0x0400	// Necessário para ativar novas funções da versão 4
+
+
+#include <windows.h>
+#include <process.h>	
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <iostream>
+#include <winsock2.h>
+#include <ctype.h>
+#include <iostream>
+#include<string>
+#pragma comment (lib, "ws2_32.lib") //Inclui a biblioteca winsocket 
 
-int main()
+using namespace std;
+using std::string;
+
+
+// Casting para terceiro e sexto parâmetros da função _beginthreadex
+typedef unsigned (WINAPI* CAST_FUNCTION)(LPVOID);
+typedef unsigned* CAST_LPDWORD;
+
+#define	ESC 	0x1B	//Tecla para encerrar o programa
+#define p       0x70    // DEfine tecla do evento p
+//----------Tamanho das mensagens-----------//
+#define TAMSTATUS 37  // 6+2+6+6+6+6 caracteres + 5 separadores
+#define TAMACK     9  // 6+2 caracteres + 1 separador
+#define TAMREQ     9  // 6+2 caracteres + 1 separador
+#define TAMPOS    41  // 6+2+6+5+5+5+6 caracteres + 6 separadores
+
+//-----Variáveis Globais----//
+int nseq = 1;
+
+//-----Estruturas 
+typedef struct TIPO11 {
+	int nseq = 1;
+	int tipo = 11;
+	int taxa = 0;
+	float potencia = 0.0;
+	float tempTrans = 0.0;
+	float tempRoda = 0.0;
+
+}TIPO11; // definição do tipo 11
+
+typedef struct TIPO33 {
+	int nseq = 1;
+	int tipo = 33;
+}TIPO33; // definição do tipo 33
+
+typedef struct TIPO99 {
+	int nseq = 1;
+	int tipo = 99;
+}TIPO99; // definição do tipo 99
+// Funções de Criação das Mensagens
+TIPO11  novaMensagem11();
+TIPO33  novaMensagem33();
+TIPO99  novaMensagem99();
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!\n";
+    //Variáveis socket//
+    WSADATA wsaData;
+    SOCKET s;
+    int statusSocket;
+    //SOCKADDR_IN ServerAddr;
+
+    //Variáveis do Temporizador//
+    HANDLE hTimer;
+    LARGE_INTEGER Present;
+
+
+
+    
+    return(0);
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
